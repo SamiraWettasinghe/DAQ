9da0405c85b66b79971df1e8e38acfcdc75ceac7
SamiraWettasinghe <sswettasinghe@gmail.com>
2020-08-13 07:32:57 -0400

    Added OBDii data.
    Through OBD, added capability to read vehicle speed and RPM. Work still needs to be done to send CAN frames to Mega successfully. OBD is accessed through Sparkfun's UART.
    
    Vehicle CAN Project - Investigation

---
+++ MLX90614/transmit/transmit.ino
@@ -0,0 +1,80 @@
+/*
+Transmit node transmit data from nano to mega.
+
+TODO:
+- Read sensor data
+- Read data from three sensors
+- Send sensor data 
+https://protosupplies.com/product/mcp2515-can-bus-interface-module/
+*/
+
+#include <SPI.h>
+#include <mcp_can.h>
+#include <i2cmaster.h>
+
+MCP_CAN CAN(10);
+byte data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Setup...");
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))
+    {
+        Serial.println("CAN Initialization Failed, you failure");
+        Serial.println("Trying again because you suck so hard...");
+        delay(2000);
+    }
+    Serial.println("CAN Shield Initialized");
+
+    i2c_init();
+    PORTC = (1 << PORTC4) | (1 << PORTC5);
+    Serial.println("i2c Initialized");
+}
+
+// Try to initialize CAN at 500k
+void loop()
+{
+    int data_low_A = 0;
+    int data_high_A = 0;
+    int pec_A = 0;
+    int data_high_B = 0;
+    int data_low_B = 0;
+    int pec_B = 0;
+
+    int dev_A = 0x55 << 1; // address location sensor 1
+    int dev_B = 0x3B << 1; // address location sesnor 2
+
+    // read sensor 1
+    i2c_start_wait(dev_A + I2C_WRITE);
+    i2c_write(0x07); // RAM location of Tobj1
+    i2c_rep_start(dev_A + I2C_READ);
+    data_low_A = i2c_readAck();
+    data_high_A = i2c_readAck();
+    pec_A = i2c_readNak();
+    i2c_stop();
+
+    // read sensor 2
+    i2c_start_wait(dev_B + I2C_WRITE);
+    i2c_write(0x07);
+    i2c_rep_start(dev_B + I2C_READ);
+    data_low_B = i2c_readAck();
+    data_high_B = i2c_readAck();
+    pec_B = i2c_readNak();
+    i2c_stop();
+
+    // send data over CAN
+    data[0] = (byte)dev_A;
+    data[1] = (byte)data_low_A;
+    data[2] = (byte)data_high_A;
+    data[3] = (byte)pec_A;
+    data[4] = (byte)dev_B;
+    data[5] = (byte)data_low_B;
+    data[6] = (byte)data_high_B;
+    data[7] = (byte)pec_B;
+
+    // CAN.sendMsgBuf(msg ID, standard frame, # of data bytes, data array)
+    CAN.sendMsgBuf(0x01, 0, 8, data);
+    delay(100);
+}
---
+++ receive/receive.ino
@@ -0,0 +1,79 @@
+/*
+Receive Node for CAN bus architecture done on Arduino Mega.
+
+https://protosupplies.com/product/mcp2515-can-bus-interface-module/
+*/
+
+#include <SPI.h>
+#include <mcp_can.h>
+
+MCP_CAN CAN(10);
+
+unsigned char len = 0; // length of received buffer
+unsigned char buf[8]; // 8 byte buffer
+unsigned int canID; // CAN Message ID
+double tempFactor = 0.02;
+int tx = 1;
+int rx = 0;
+
+// Try to initialize CAN at 500k
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(tx, OUTPUT);
+    pinMode(rx, INPUT);
+
+    if (Serial.available() > 0) {
+        Serial.println("Serial monitor available");
+    }
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))
+    {
+        Serial.println("CAN Initialization Failed, you failure");
+        Serial.println("Trying again because you suck so hard...");
+        delay(2000);
+    }
+    Serial.println("CAN Shield Initialized");
+}
+
+void loop()
+{
+    if (CAN.checkReceive() == 3) { // 3 Means CAN_MSGAVAIL
+        CAN.readMsgBuf(&len, buf); // len: data length, buf: data buffer
+        canID = CAN.getCanId();
+
+        Serial.print("CAN ID: ");
+        Serial.print(canID, HEX);
+
+        Serial.print("  Data Length: ");
+        Serial.print(len);
+        Serial.print("  ");
+
+        // Print the data one byte at a time
+        for (int i = 0; i<len; i++) {
+            Serial.print(int(buf[i]));
+            if(i<(len-1)) Serial.print(",");
+        }
+        Serial.println();
+
+        // Sensor A
+        double tempData_A = 0x0000; // zero out the data
+        tempData_A = (double)(((buf[2] & 0x007F) << 8) + buf[1]);
+        tempData_A = (tempData_A * tempFactor) - 0.01;
+        float celcius_A = tempData_A - 273.15;
+        Serial.print("Celcius Sensor A: ");
+        Serial.println(celcius_A);
+
+        // Sensor B
+        double tempData_B = 0x0000; // zero out the data
+        tempData_B = (double)(((buf[6] & 0x007F) << 8) + buf[5]);
+        tempData_B = (tempData_B * tempFactor) - 0.01;
+        float celcius_B = tempData_B - 273.15;
+        Serial.print("Celcius Sensor B: ");
+        Serial.println(celcius_B);
+    }
+    else {
+        Serial.println("No Message");
+        delay(10000);
+    }
+}
