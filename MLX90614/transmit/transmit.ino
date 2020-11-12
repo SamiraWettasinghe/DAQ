1482bc9c021248139cb8849787f555647583c95b
SamiraWettasinghe <sswettasinghe@gmail.com>
2020-11-12 08:26:08 -0500

    Differential wire concept
    Testing concept with serial comms.
    
    2.2 Basic Sensor Suite

--- MLX90614/transmit/transmit.ino
+++ MLX90614/transmit/transmit.ino
@@ -1,20 +1,6 @@
 /*
 Transmit node transmit data from tire temp nanos to mega.
-
-TODO:
-- Read sensor data
-- Send sensor data 
 */
-
-#include <SPI.h>
-#include <mcp_can.h>
-#include <i2cmaster.h>
-
-MCP_CAN CAN(10);
-byte data[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-unsigned long time;
-byte timeBuffer[4] = {0, 0, 0, 0};
 
 int dev_A = 0x3A << 1; // address location sensor 1
 int dev_B = 0x4B << 1; // address location sesnor 2
@@ -24,70 +10,54 @@ void setup()
     Serial.begin(9600);
     Serial.println("Setup...");
 
-    while (CAN_OK != CAN.begin(CAN_500KBPS))
-    {
-        Serial.println("CAN Initialization Failed, you failure");
-        Serial.println("Trying again because you suck so hard...");
-        delay(2000);
-    }
-    Serial.println("CAN Shield Initialized");
+    pinMode(2, OUTPUT);
 
-    i2c_init();
-    PORTC = (1 << PORTC4) | (1 << PORTC5);
-    Serial.println("i2c Initialized");
+//    i2c_init();
+//    PORTC = (1 << PORTC4) | (1 << PORTC5);
+//    Serial.println("i2c Initialized");
 }
 
-// Try to initialize CAN at 500k
 void loop()
 {
-    int data_low_A = 0;
-    int data_high_A = 0;
-    int pec_A = 0;
-    int data_high_B = 0;
-    int data_low_B = 0;
-    int pec_B = 0;
+//    int data_low_A = 0;
+//    int data_high_A = 0;
+//    int pec_A = 0;
+//    int data_high_B = 0;
+//    int data_low_B = 0;
+//    int pec_B = 0;
+//
+//    // read sensor 1
+//    i2c_start_wait(dev_A + I2C_WRITE);
+//    i2c_write(0x07); // RAM location of Tobj1
+//    i2c_rep_start(dev_A + I2C_READ);
+//    data_low_A = i2c_readAck();
+//    data_high_A = i2c_readAck();
+//    pec_A = i2c_readNak();
+//    i2c_stop();
+//    Serial.print("A ");
+//    Serial.print(data_low_A);
+//    Serial.println();
+//
+//    // read sensor 2
+//    i2c_start_wait(dev_B + I2C_WRITE);
+//    i2c_write(0x07);
+//    i2c_rep_start(dev_B + I2C_READ);
+//    data_low_B = i2c_readAck();
+//    data_high_B = i2c_readAck();
+//    pec_B = i2c_readNak();
+//    i2c_stop();
+//    Serial.print("B ");
+//    Serial.print(data_low_B);
+//    Serial.println();
+//
+//    time = millis();
+//    timeBuffer[0] = time;
+//    timeBuffer[1] = time >> 8;
+//    timeBuffer[2] = time >> 16;
+//    timeBuffer[3] = time >> 24;
 
-    // read sensor 1
-    i2c_start_wait(dev_A + I2C_WRITE);
-    i2c_write(0x07); // RAM location of Tobj1
-    i2c_rep_start(dev_A + I2C_READ);
-    data_low_A = i2c_readAck();
-    data_high_A = i2c_readAck();
-    pec_A = i2c_readNak();
-    i2c_stop();
-    Serial.print("A ");
-    Serial.print(data_low_A);
-    Serial.println();
-
-    // read sensor 2
-    i2c_start_wait(dev_B + I2C_WRITE);
-    i2c_write(0x07);
-    i2c_rep_start(dev_B + I2C_READ);
-    data_low_B = i2c_readAck();
-    data_high_B = i2c_readAck();
-    pec_B = i2c_readNak();
-    i2c_stop();
-    Serial.print("B ");
-    Serial.print(data_low_B);
-    Serial.println();
-
-    time = millis();
-    timeBuffer[0] = time;
-    timeBuffer[1] = time >> 8;
-    timeBuffer[2] = time >> 16;
-    timeBuffer[3] = time >> 24;
-
-    // send data over CAN
-    data[0] = (byte)timeBuffer[0];
-    data[1] = (byte)timeBuffer[1];
-    data[2] = (byte)timeBuffer[2];
-    data[3] = (byte)timeBuffer[3];
-    data[4] = (byte)data_low_A;
-    data[5] = (byte)data_high_A;
-    data[6] = (byte)data_low_B;
-    data[7] = (byte)data_high_B;
-
-    // CAN.sendMsgBuf(msg ID, standard frame, # of data bytes, data array)
-    CAN.sendMsgBuf(0x01, 0, 8, data);
-    delay(2);
+    digitalWrite(2, HIGH);
+    Serial.write(69);
+    digitalWrite(2, LOW);
+    delay(100);
 }
